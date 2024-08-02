a57731a87e8ce4168b9581fe85bdd88e2b71011d
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-08-01 23:59:22 -0400

    Finished Basic Layout Website
    I finished designing the basic layout of the website for
    registering user with Bootstrap.

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-class MainApp:
+app = Flask(__name__)
 
-    def __init__(self):
-        self.__app = Flask(__name__)
-        self.__setup_routes()
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    def __setup_routes(self):
-        @self.__app.route("/")
-        def index():
-            return render_template("index.html")
-
-    def run_app(self):
-        self.__app.run()
-
-# Runs the flask application
-if __name__ == "__main__":
-    MainApp().run_app()
+if __name__ == '__main__':
+    app.run(debug=True)
