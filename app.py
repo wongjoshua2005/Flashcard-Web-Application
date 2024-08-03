df67c79f8164d8eb77753b50ce132505b861764e
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-08-02 23:13:09 -0400

    Finalized Register and Login Page
    I finalized the register and login designs. I am
    now going to work on allowing the user to enter in their information
    and register into my application.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 
 app = Flask(__name__)
@@ -6,28 +6,31 @@ app = Flask(__name__)
 # Session information
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-user_signed = False
+app.secret_key = 'supersecretkey'
+Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        global user_signed
-        user_signed = True
-        return redirect("/home")
-    
-    user_signed = False
-    return render_template("register.html", logged=user_signed)
+        session["user_signed"] = True
+        return redirect("/sets")
 
-@app.route("/home")
-def home():
-    return render_template("index.html", logged=user_signed)
+    session["user_signed"] = False
+    return render_template("register.html", logged=session.get("user_signed"))
+
+@app.route("/sets")
+def user_sets():
+    return render_template("index.html", logged=session.get("user_signed"))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        global user_signed
-        user_signed = True
-        return redirect("/home")
+        session["user_signed"] = True
+        return redirect("/sets")
 
-    user_signed = False
-    return render_template("login.html")
+    return render_template("login.html", logged=session.get("user_signed"))
+
+
+# Debuggging purposes
+if __name__ == "__main__":
+    app.run(debug=True)
