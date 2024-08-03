55c3454abed79efa96ffe7c093fef7c3f1ae14b1
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-08-02 22:55:48 -0400

    Implemented ability to login or register to the home page
    I implemented the ability to login and register and redirect
    the user to the homepage successfully.

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request, redirect
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Session information
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+user_signed = False
 
 @app.route("/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        global user_signed
+        user_signed = True
         return redirect("/home")
     
-    return render_template("register.html")
+    user_signed = False
+    return render_template("register.html", logged=user_signed)
 
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged=user_signed)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        global user_signed
+        user_signed = True
+        return redirect("/home")
+
+    user_signed = False
     return render_template("login.html")
