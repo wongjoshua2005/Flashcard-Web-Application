fcd5d4373ec2d9c83f56f37ca9baddcb70116a65
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-08-03 22:30:35 -0400

    Added implementation to give error messages
    I added an implementation to redirect user
    if they made an error when registering their
    information. In addition, I properly
    connected my database to Flask.

--- app.py
+++ app.py
@@ -1,35 +1,76 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, g, url_for
 from flask_session import Session
+import sqlite3
 
 app = Flask(__name__)
+
+DATABASE = "user_database.db"
+# Database connection information
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 # Session information
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.secret_key = 'supersecretkey'
 Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        session["user_signed"] = True
+        user_name = request.form.get("username")
+        password = request.form.get("password")
+        confirm_pass = request.form.get("confirm_pass")
+
+        if not user_name or not password or not confirm_pass:
+            error_msg = "404...INVALID USERNAME OR PASSWORD >:C"
+            return redirect(url_for("error", message=error_msg))
+
+        if password != confirm_pass:
+            error_msg = "404...Invalid Password Verification!!! :C"
+            return redirect(url_for("error", message=error_msg))
+        
+        
+        
+
+
+        print(user_name)
+        print(password)
+        print(confirm_pass)
+
         return redirect("/sets")
 
-    session["user_signed"] = False
-    return render_template("register.html", logged=session.get("user_signed"))
-
-@app.route("/sets")
-def user_sets():
-    return render_template("index.html", logged=session.get("user_signed"))
+    return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
+    # Closes out any user information logged in
+    session.clear()
+
     if request.method == "POST":
-        session["user_signed"] = True
         return redirect("/sets")
 
-    return render_template("login.html", logged=session.get("user_signed"))
+    return render_template("login.html")
 
+@app.route("/error")
+def error():
+    error_reason = request.args.get("message")
+    return render_template("error.html", code=error_reason)
+    
+
+@app.route("/sets")
+def user_sets():
+    return render_template("index.html")
 
 # Debuggging purposes
 if __name__ == "__main__":
