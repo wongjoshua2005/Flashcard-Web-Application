168e942b797149b89ef7591ce3bf3809479d7e11
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-08-08 23:42:34 -0400

    Finished fixing my pomodoro timer
    I finished the pomodoro timer to
    work with AudioContext class.
    It's safe for me to assume
    that I can now start fixing up
    the main backend to be more
    organized. My biggest
    mistake is not doing this early.

--- static/timer.js
+++ static/timer.js
@@ -1,26 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() 
+document.addEventListener("DOMContentLoaded", () =>
 {
     const button = document.querySelector(".timer");
     const display_time = document.querySelector(".time");
     const progress = document.querySelector(".progress_track");
+    const original_title = document.title;
+    let check_distraction = false;
+
+    const audioCtx = new AudioContext();
+    let oscillatorNode;
+
+    document.addEventListener("visibilitychange", (event) => {
+        if (document.visibilityState != "visible" && check_distraction) {
+            document.title = "YOU'RE DISTRACTED!!!";
+            reset_timer = true;
+            oscillatorNode = audioCtx.createOscillator();
+            oscillatorNode.type = "sine";
+            oscillatorNode.frequency.value = 800;
+            oscillatorNode.connect(audioCtx.destination);
+            oscillatorNode.start();
+        } else {
+            document.title = original_title;
+            if (oscillatorNode) {
+                oscillatorNode.stop();
+                oscillatorNode = null;
+            }
+        }
+    });
+
 
     const POMO_START = 25;
     const SHORT_BREAK = 5;
     const LONG_BREAK = 10;
     const FIXED_SECS = 0;
+    let reset_timer = false;
 
     let time = new Date();
     time.setMinutes(POMO_START);
     time.setSeconds(FIXED_SECS);
     let count_progress = 0;
 
-    button.addEventListener("click", function()
+    button.addEventListener("click", () =>
     {  
-        console.log("i am running!!!")
+        check_distraction = true;
         let curr_mins = time.getMinutes();
         let curr_secs = time.getSeconds();
 
-        setInterval(function() 
+        setInterval(() =>
         {
             display_time.textContent = String(curr_mins).padStart(2, '0') + ":"
             + String(curr_secs).padStart(2, '0');
@@ -38,6 +63,13 @@ document.addEventListener("DOMContentLoaded", function()
                 curr_mins = SHORT_BREAK;
                 curr_secs = FIXED_SECS;
                 count_progress += 1
+            }
+            else if (reset_timer)
+            {
+                curr_mins = POMO_START;
+                curr_secs = FIXED_SECS;
+                count_progress = 0;
+                reset_timer = false;
             }
             else
             {
